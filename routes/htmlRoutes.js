de207c920e0869569bd5c177dde263b76efbf313
David Tierney <daveshouse44@hotmail.com>
2021-09-09 00:19:01 -0500

    route bug fix

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = (app) =>{
 // GET Route for homepage
-    app.get('/', (req, res) =>
+    app.get('*', (req, res) =>
     res.sendFile(path.join(__dirname, '../public/index.html'))
     );
 
@@ -10,10 +10,5 @@ module.exports = (app) =>{
     app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '../public/notes.html'))
     );
-
-    // GET Route for error page
-    app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/404.html'))
-    })
 
 };
